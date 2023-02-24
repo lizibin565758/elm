a22fcdee29aeffec99a76624edaca39e0587f205
Lizb <931368458@qq.com>
2023-02-24 14:52:00 +0800

    更新导航栏组件

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {createPinia} from 'pinia'
 import App from './App.vue'
 // import router from './router'
 
+// 2. 引入组件样式
 import 'vant/lib/index.css'
 // import './assets/main.css'
 
--- src/views/tabs/TabsView.vue
+++ src/views/tabs/TabsView.vue
@@ -1,27 +1,31 @@
 <!--
  * @FileDescription: 导航栏组件
+ *
  * @Author: Lizb
  * @Date: 2023-02-24 14:26:59
  -->
 <template>
+  <div class="center">{{ active }}</div>
   <Tabbar v-model="active">
-    <TabbarIteme name="home" icon="home-o">首页</TabbarIteme>
-    <TabbarIteme name="order" icon="bars">订单</TabbarIteme>
-    <TabbarIteme name="mine" icon="contact">我的</TabbarIteme>
+    <TabbarItem name="home" icon="home-o">首页</TabbarItem>
+    <TabbarItem name="order" icon="bars">订单</TabbarItem>
+    <TabbarItem name="mine" icon="contact">我的</TabbarItem>
   </Tabbar>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
-import {Tabbar} from "vant";
+import { ref } from 'vue'
+import { Tabbar, TabbarItem } from "vant";
 
 
 // 记录当前选择的tab
-const active = ref('')
+const active = ref('home')
 
 
 </script>
 
 <style scoped>
-
+.center {
+  text-align: center;
+}
 </style>
