079f4e473ad0d1af3181f2b010b784ed340cba0e
Lizb <931368458@qq.com>
2023-02-24 17:40:36 +0800

    配置vite反向代理

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,18 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), vueJsx()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    // 配置vite代理
+    server: {
+        port: 3000, // 把前端项目端口改成3000
+        proxy: { // proxy是用来配置代理的
+            '/api': 'http://localhost:8000',
+            '/imgs': 'http://localhost:8000',
+        }
     }
-  }
 })
-//https://github.com/lizibin565758/elm-h5.git
